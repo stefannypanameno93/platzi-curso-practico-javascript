0b99a013fa9c15ac0b431929cd628d4954e4030e
Stefanny Panameño <slpn93@gmail.com>
2021-08-04 23:25:33 -0600

    AUtilizando reduce e insertando una función

--- promedio.js
+++ promedio.js
@@ -11,6 +11,12 @@ function calcularMediaAritmetica(lista){
     // for (let i = 0; i < lista.length; i++) {
     //     sumaLista = sumaLista + lista[i];
     // }
+
+    const sumaLista = lista.reduce(
+        function (valorAcumulado=0, nuevoElemento) {
+            return valorAcumulado + nuevoElemento
+        }
+    );
     
     const promedioLista = sumaLista / lista.length;
 
